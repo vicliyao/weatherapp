f856f21e54ed90e4e713adf40b43cbea18fae1fb
Yao(Victor) Li <victorliyao@gmail.com>
2023-04-27 21:44:30 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 window.onload = function(){
     const date=new Date();
     const dateString = (date.getMonth()+1)+"/" + date.getDate() + "/" + date.getFullYear();
-
+    document.getElementById("date").innerHTML = dateString;
 
     if ("geolocation" in navigator){
         navigator.geolocation.getCurrentPosition(success);
@@ -18,5 +18,15 @@ window.onload = function(){
 function success(position){
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
-    
+    console.log(latitude,longitude);
 }
+
+const btn = document.getElementById('getWeatherBtn');
+
+btn.addEventListener('click',()=>{
+    let forecast = [['M',52],["Tu",53],['W',54],['Th',55],['F',56]]
+    let forecastElements = document.getElementsByClassName('forecast');
+    for (let i=0; i<forecast.length; i++){
+        forecastElements[i].innerHTML = forecast[i][0] + ':' + forecast[i][1] + '°F';
+    }
+});
